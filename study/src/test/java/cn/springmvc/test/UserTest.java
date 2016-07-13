3bd9024b6cda03f6dadc4262d389fde690db0826
= <=>
2016-07-13 18:29:20 +0800

    modify mybatis-config and mapper

--- study/src/test/java/cn/springmvc/test/UserTest.java
+++ study/src/test/java/cn/springmvc/test/UserTest.java
@@ -4,7 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import
 import cn.springmvc.model.User;
 import cn.springmvc.service.UserService;
 
